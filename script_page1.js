9afce059a5ea5392b0b6ea7f4033aadafe9ed092
RYUOTAMA <chayut.r@tgbc.ac.th>
2024-12-22 21:31:49 +0700

    212

--- script_page1.js
+++ script_page1.js
@@ -5,12 +5,19 @@ const dropdown_Prefix = document.getElementById('dropdown_Prefix');
 const Name_Surname = document.getElementById('Name_Surname');
 const Student_ID = document.getElementById('Student_ID');
 const submit_btn = document.getElementById('submit-btn');
+const row = document.createElement("tr");
 
-function myFunction() {
-    document.getElementById("submit-btn").submit();
-  }
+function sendData() {
+  window.location.href = 'page2.html'; // เปลี่ยนไปยังหน้าอื่น
+}
 
-
-form.addEventListener('submit',addTransaction);
-
+row.innerHTML = `
+<td>${table.rows.length + 1}</td>
+<td>${dropdown_equipment}</td>
+<td>${dropdown_GradeLevel}</td>
+<td>${dropdown_Prefix}</td>
+<td>${Name_Surname}</td>
+<td>${Student_ID}</td>
+<td><button class="delete-btn">ลบ</button></td>
+`;
 
