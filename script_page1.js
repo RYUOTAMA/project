5663f8d4b0aeb3ca6ad4a73d3b7cc4b4e7963272
RYUOTAMA <chayut.r@tgbc.ac.th>
2024-12-23 21:01:50 +0700

    2321

--- script_page1.js
+++ script_page1.js
@@ -2,15 +2,16 @@
 function saveBorrowData() {
   const dropdownequipment = document.getElementById('dropdown_equipment').value;
   const dropdownGradeLevel = document.getElementById('dropdown_GradeLevel').value;
-  const dropdownPrefix = document.getElementById('dropdown_Prefix').value;
+  /* const dropdownPrefix = document.getElementById('dropdown_Prefix').value; */
   const NameSurname = document.getElementById('Name_Surname').value;
   const StudentID = document.getElementById('Student_ID').value;
   const dateStart = document.getElementsByClassName('field')[0].value;
   const dateEnd = document.getElementsByClassName('field')[1].value;
+  const date = document.getElementsByClassName('field')[2].value;
   const timeStart = document.getElementsByClassName('field')[2].value;
   const timeEnd = document.getElementsByClassName('field')[3].value;
 
-  if (!dropdownequipment || !dropdownGradeLevel || !dropdownPrefix || !NameSurname || !StudentID || !dateStart || !dateEnd || !timeStart || !timeEnd) {
+  if (!dropdownequipment || !dropdownGradeLevel /* || !dropdownPrefix */ || !NameSurname || !StudentID || !dateStart || !dateEnd || !timeStart || !timeEnd) {
     alert('กรุณากรอกข้อมูลให้ครบถ้วน');
     return;
   }
@@ -18,7 +19,7 @@ function saveBorrowData() {
   const borrowData = {
     dropdownequipment,
     dropdownGradeLevel,
-    dropdownPrefix,
+  /*   dropdownPrefix, */
     NameSurname,
     StudentID,
     date: `${dateStart} - ${dateEnd}`,
@@ -28,5 +29,5 @@ function saveBorrowData() {
   let borrowRecords = JSON.parse(localStorage.getItem('borrowRecords')) || [];
   borrowRecords.push(borrowData);
   localStorage.setItem('borrowRecords', JSON.stringify(borrowRecords));
-  window.location.href = 'we1.html';
+  window.location.href = 'page2.html';
 }
