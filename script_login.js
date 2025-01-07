71419823d47999a3d56409a70d922a6ac3574614
castrolYAIMAK <tharathep.p2549@gmail.com>
2025-01-07 18:25:48 +0700

    00000

--- script_login.js
+++ script_login.js
@@ -1,17 +1,24 @@
 var objpeople = [
     {
-        username: "heeyaimak",
+        username: "thai go",
         password: "HEE"
     }    
 ];
 
 function getInfo() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var username = document.getElementById("username")?.value;
+    var password = document.getElementById("password")?.value;
+
+    var isMatch = false;
 
     for (i = 0; i < objpeople.length; i++) {
         if (username == objpeople[i].username && password == objpeople[i].password){
-            console.log(HEE);
+            window.location.href = 'index.html';
+            isMatch = true;
+            break;
         }
+    }
+    if (!isMatch) {
+        alert("Username หรือ Password ไม่ถูกต้อง!");
     }
 }
